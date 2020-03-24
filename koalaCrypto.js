34ce40d96039e26b1cf7c291a6fe0c86df2722e2
zicodes <info@mrzilvi.net>
2020-03-24 00:26:44 +0000

    initial commit

---
+++ koalaCrypto.js
@@ -0,0 +1,5 @@
+exports.handler = async (event) => {
+    const bcrypt = require('bcrypt')
+    let hash = bcrypt.hashSync('AFDSFJLASJFOIJQWOJFOIQJWOIFJQWJOIFJQWIO5236532462652ADFKLMSKDANFLWENLFJEWNTASJFL523523dassaddasKA532JFQfsadjkfasf', 12);
+    return {"message": hash};
+};
